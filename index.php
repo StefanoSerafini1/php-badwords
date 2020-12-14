6cfd90997f54271e5f07e435bb22487b4a861bd7
StefanoSerafini <stefanoserafini7@gmail.com>
2020-12-14 17:30:12 +0100

    end exercise

--- index.php
+++ index.php
@@ -9,9 +9,14 @@
 
     <h1>Paragrafo PHP</h1>
     <?php
-        $paragrafo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        $paragrafo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.';
+        $parola_proibita=  $_GET['badword']; 
+        $modifica= str_replace($parola_proibita , '*****' , $paragrafo);
     ?>
 
-    <p><?php echo $paragrafo ?></p>
+    <p><?php echo $paragrafo; ?></p>
+    <p><?php echo $modifica; ?></p>
+    <h2>Lunghezza stringa paragrafo : <?php echo strlen($paragrafo); ?></h2>
+    <h2>Parola da modificare: <?php echo $parola_proibita; ?></h2>
 </body>
 </html>
