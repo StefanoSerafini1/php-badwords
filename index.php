dc15208ec24ff02f1f355d547d71fb93f5c68ec1
StefanoSerafini <stefanoserafini7@gmail.com>
2020-12-14 17:06:30 +0100

    paragrafo a schermo

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>first step of PHP</title>
+</head>
+<body>
+
+    <h1>Paragrafo PHP</h1>
+    <?php
+        $paragrafo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    ?>
+
+    <p><?php echo $paragrafo ?></p>
+</body>
+</html>
